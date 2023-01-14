85d8696a921fcac06320d37670313c3d07a76ae2
jpsantos9 <jasantos9@upm.edu.ph>
2023-01-14 10:01:27 +0800

    Fix issue jpa and spring

--- sample.spring/src/main/java/com/example/sample/spring/service/VersionService.java
+++ sample.spring/src/main/java/com/example/sample/spring/service/VersionService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class VersionService {
 	public String getServerVersion() {
+		System.out.println("Someone retrieved the version");
 		return "2.0.0";
 	}
 }
